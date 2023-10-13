0f4ba6b4308cdb4911e82ad54c6ac14ba7130250
glansa <glansa.solutions777@gmail.com>
2023-10-14 01:01:49 +0530

    Vishal Committed

---
+++ lms2/about.php
@@ -0,0 +1,22 @@
+<?php
+include('includes/header.php');
+include('includes/sidebar.php');
+?>
+
+
+<!-- Main Content Panel -->
+        <div class="content-wrapper">
+            <div class="row">
+                <div class="col-sm-12">
+                    <!-- What ever creating, create here -->
+                    <h3>About Page</h3>
+                </div>
+            </div>
+        </div>
+<!-- Main Content ends -->
+
+<?php
+
+include('includes/footer.php');
+
+?>
---
+++ lms2/includes/sidebar.php
@@ -0,0 +1,182 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?=$mainlink?>dashboard">
+                            <i class="mdi mdi-grid-large menu-icon"></i>
+                            <span class="menu-title">Dashboard</span>
+                        </a>
+                    </li>
+                    <!-- <li class="nav-item nav-category">UI Elements</li>   -->
+                    <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false"
+                            aria-controls="ui-basic">
+                            <i class="menu-icon mdi mdi-floor-plan"></i>
+                            <!-- <span class="menu-title">UI Elements</span> -->
+                            <span class="menu-title">Site Pages</span>
+                            <i class="menu-arrow"></i>
+                        </a>
+                        <div class="collapse" id="ui-basic">
+                            <ul class="nav flex-column sub-menu">
+                                <!-- <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
+                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a></li>
+                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li> -->
+                                <li class="nav-item"> <a class="nav-link" href="<?=$mainlink?>home">Home</a>
+                                </li>
+                                <li class="nav-item"> <a class="nav-link"
+                                        href="<?=$mainlink?>about">About</a></li>
+                                <li class="nav-item"> <a class="nav-link"
+                                        href="<?=$mainlink?>contact">Contact Details</a></li>
+                                <li class="nav-item"> <a class="nav-link"
+                                        href="<?=$mainlink?>privacy">Privacy policy</a></li>
+                                <li class="nav-item"> <a class="nav-link" href="<?=$mainlink?>terms">Terms
+                                        & Conditions</a></li>
+                                <li class="nav-item"> <a class="nav-link"
+                                        href="<?=$mainlink?>development">Sustainable Development</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <!-- <li class="nav-item nav-category">Forms and Datas</li> -->
+                    <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="collapse" href="#form-elements" aria-expanded="false"
+                            aria-controls="form-elements">
+                            <i class="menu-icon mdi mdi-card-text-outline"></i>
+                            <!-- <span class="menu-title">Form elements</span> -->
+                            <span class="menu-title">Master</span>
+                            <i class="menu-arrow"></i>
+                        </a>
+                        <div class="collapse" id="form-elements">
+                            <ul class="nav flex-column sub-menu">
+                                <!-- <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Basic Elements</a></li> -->
+                                <li class="nav-item"><a class="nav-link"
+                                        href="pages/forms/basic_elements.html">Topics</a></li>
+                                <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Sub
+                                        Topic admin</a></li>
+                                <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Manage
+                                        Courses</a></li>
+                                <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Manage
+                                        Students</a></li>
+                                <li class="nav-item"><a class="nav-link"
+                                        href="pages/forms/basic_elements.html">Subscriptions</a></li>
+                                <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Student
+                                        Subscriptions</a></li>
+                                <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Bulk
+                                        registrations</a></li>
+                                <li class="nav-item"><a class="nav-link"
+                                        href="pages/forms/basic_elements.html">Payments</a></li>
+                                <li class="nav-item"><a class="nav-link"
+                                        href="pages/forms/basic_elements.html">Users</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="collapse" href="#charts" aria-expanded="false"
+                            aria-controls="charts">
+                            <i class="menu-icon mdi mdi-chart-line"></i>
+                            <!-- <span class="menu-title">Charts</span> -->
+                            <span class="menu-title">Reports</span>
+                            <i class="menu-arrow"></i>
+                        </a>
+                        <div class="collapse" id="charts">
+                            <ul class="nav flex-column sub-menu">
+                                <!-- <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">ChartJs</a></li> -->
+                                <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">MIS</a></li>
+                                <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">Daily
+                                        Payment</a></li>
+                                <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">Pending
+                                        Subscriptions</a></li>
+                                <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">Courses
+                                        List</a></li>
+                                <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">Students
+                                        List</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"
+                            href="#">
+                            <i class="menu-icon mdi mdi-file-document"></i>
+                            <span class="menu-title">Blog</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"
+                            href="#">
+                            <i class="menu-icon mdi mdi-file-document"></i>
+                            <span class="menu-title">Free resources</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"
+                            href="#">
+                            <i class="menu-icon mdi mdi-file-document"></i>
+                            <span class="menu-title">Affiliate</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"
+                            href="#">
+                            <i class="menu-icon mdi mdi-file-document"></i>
+                            <span class="menu-title">Career</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"
+                            href="#">
+                            <i class="menu-icon mdi mdi-file-document"></i>
+                            <span class="menu-title">Corporate Governance</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="collapse" href="#tables" aria-expanded="false"
+                            aria-controls="tables">
+                            <i class="menu-icon mdi mdi-table"></i>
+                            <!-- <span class="menu-title">Tables</span> -->
+                            <span class="menu-title">Auth</span>
+                            <i class="menu-arrow"></i>
+                        </a>
+                        <div class="collapse" id="tables">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item"> <a class="nav-link" href="pages/tables/basic-table.html">Forgot
+                                        Password</a></li>
+                            </ul>
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item"> <a class="nav-link" href="pages/tables/basic-table.html">Change
+                                        Password</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
+              <i class="menu-icon mdi mdi-layers-outline"></i>
+              <span class="menu-title">Icons</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="icons">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="pages/icons/mdi.html">Mdi icons</a></li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item nav-category">pages</li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+              <i class="menu-icon mdi mdi-account-circle-outline"></i>
+              <span class="menu-title">User Pages</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="auth">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item nav-category">help</li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="menu-icon mdi mdi-file-document"></i>
+              <span class="menu-title">Documentation</span>
+            </a>
+          </li>
+        </ul>
+            </nav>
+            <div class="main-panel">
