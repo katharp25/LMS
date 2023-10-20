c377da74045c265fea430f994487df73923a4108
DESKTOP-D1RQIAA\Pradip <pradip.kathar@paexskin.com>
2023-10-20 09:28:21 +0530

    topic and subtopic posting data

--- lms2/functions/list_grid.php
+++ lms2/functions/list_grid.php
@@ -5,6 +5,8 @@ include('config.php');
 // fetching list of Users to users module
 
 $fetch_list_query=mysqli_query($con,"SELECT * FROM users");
+$fetch_list_topic_query=mysqli_query($con,"SELECT * FROM topics");
+$fetch_list_subtopic_query=mysqli_query($con,"SELECT * FROM topics");
 // $fetch_list=mysqli_fetch_assoc($fetch_list_query);
 // $users_name=$fetch_list['Name'];
 // echo $users_name;
